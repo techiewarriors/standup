bd4c2c1066838cccd42130f8508b059e765363ba
sunilverma209 <sunil.verma@boxuk.com>
2022-02-19 17:38:47 +0000

    [TW-WDM][STANDUP] - Intial Commit - Fixing the accidentialy commited files

--- assets/setup/env.php
+++ assets/setup/env.php
@@ -7,14 +7,14 @@ if (!defined('APP_DESCRIPTION'))                define('APP_DESCRIPTION', 'Stand
 
 if (!defined('ALLOWED_INACTIVITY_TIME'))        define('ALLOWED_INACTIVITY_TIME', time()+1*60);
 
-if (!defined('DB_DATABASE'))                    define('DB_DATABASE', 'wrqtepbzfx');
+if (!defined('DB_DATABASE'))                    define('DB_DATABASE', '');
 if (!defined('DB_HOST'))                        define('DB_HOST','localhost');
-if (!defined('DB_USERNAME'))                    define('DB_USERNAME','wrqtepbzfx');
-if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'J26RqgPKfq');
+if (!defined('DB_USERNAME'))                    define('DB_USERNAME','');
+if (!defined('DB_PASSWORD'))                    define('DB_PASSWORD' ,'');
 if (!defined('DB_PORT'))                        define('DB_PORT' ,'');
 
 if (!defined('MAIL_HOST'))                      define('MAIL_HOST', 'smtp.gmail.com');
-if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'sujoinhouse@gmail.com');
-if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', 'sujoin@DEV');
+if (!defined('MAIL_USERNAME'))                  define('MAIL_USERNAME', 'xxx@gmail.com');
+if (!defined('MAIL_PASSWORD'))                  define('MAIL_PASSWORD', '');
 if (!defined('MAIL_ENCRYPTION'))                define('MAIL_ENCRYPTION', 'tls'); // tls  // ssl
 if (!defined('MAIL_PORT'))                      define('MAIL_PORT', 587); // 587 //  465
